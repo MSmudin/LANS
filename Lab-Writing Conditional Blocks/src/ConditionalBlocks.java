8a1b26c774bc6acc0fb4df0ae631c10e347e1b8b
Alexandra <anance5@murraystate.edu>
2021-06-09 13:56:59 -0500

    Adding Conditional Blocks Labs to alex-branch

---
+++ Lab-Writing Conditional Blocks/src/ConditionalBlocks.java
@@ -0,0 +1,13 @@
+
+public class ConditionalBlocks {
+
+	public static void main(String[] args) {
+		boolean b = true;
+		
+		if(b) {
+			System.out.println("Inside the if-statement");
+		}
+		
+		System.out.println("Outside the if-statement");
+	}
+}
