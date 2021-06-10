c1bfcde005dec5b7b44061b97567dc86d6b8ac79
MohammadRAmjad <85632705+MohammadRAmjad@users.noreply.github.com>
2021-06-10 13:55:31 -0700

    Update FB.java

--- FooBar/src/FB.java
+++ FooBar/src/FB.java
@@ -1,7 +1,7 @@
 
 public class FB {
 	public static void main(String[] args) {
-		for(int i = 0; i <= 100; i++) {
+		for(int i = 1; i <= 100; i++) {
 			if((i % 3) == 0 && (i % 5) == 0) {
 				System.out.print("value of i is: " + i);
 				System.out.print("   ");
